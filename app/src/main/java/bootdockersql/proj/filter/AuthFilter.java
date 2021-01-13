23d012b3361d0d535edc186d15b10e8ec900adc9
YaTut <ovanya1901@gmail.com>
2021-01-13 13:52:45 +0200

    authentication_in_app

---
+++ app/src/main/java/bootdockersql/proj/filter/AuthFilter.java
@@ -0,0 +1,39 @@
+package bootdockersql.proj.filter;
+
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+@Component
+@Order(1)
+public class AuthFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+        String method = httpServletRequest.getMethod();
+        int responseCode = getResponseCode(method);
+        if (responseCode == 401) {
+            ((HttpServletResponse) servletResponse).sendError(HttpServletResponse.SC_UNAUTHORIZED);
+        } else {
+            filterChain.doFilter(servletRequest, servletResponse);
+        }
+    }
+
+    private int getResponseCode(String method) throws IOException {
+        URL url = new URL("http://authentication:8080/" + "?param=" + (method.equals("POST") ? "OK" : "NOTOK"));
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("GET");
+        return con.getResponseCode();
+    }
+}
+
--- authentication/src/main/java/com/example/demo/controller/AuthController.java
+++ authentication/src/main/java/com/example/demo/controller/AuthController.java
@@ -3,15 +3,17 @@ package com.example.demo.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+@RequestMapping("/")
 @Controller
 public class AuthController {
-    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @GetMapping
     @ResponseBody
-    public ResponseEntity method() {
-        return new ResponseEntity(HttpStatus.UNAUTHORIZED);
+    public ResponseEntity method(@RequestParam String param) {
+        return param.equals("OK") ? new ResponseEntity(HttpStatus.OK) : new ResponseEntity(HttpStatus.UNAUTHORIZED);
     }
 }
